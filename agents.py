71ba01e075f67c8e3cb138562052810b380916d2
Jackeurey <jackcolemancs@gmail.com>
2025-10-16 19:12:06 -0400

    Revert "changed to update policy explictly"
    This reverts commit 38fec833ba1bd3a1537e631f75738837cb34e86d.

--- agents.py
+++ agents.py
@@ -97,12 +97,9 @@ class PolicyIterationAgent(ValueIterationAgent):
         converged = False  # {state: False for state in self.values.keys()}
         while not converged:  # all(converged.values()):
             for state in self.values.keys():
+                if converged[state]:
+                    continue
                 policy_values[state] = self.get_q_value(state, polices[state])
-                new_policy = self.get_best_policy(state)
-                new_q = self.get_q_value(state, new_policy)
-                if new_q > policy_values[state]:
-                    polices[state] = new_policy
-                    policy_values[state] = new_q
                 if abs(policy_values[state] - self.values[state]) < epsilon:
                     converged = True  # converged[state] = True
             self.values = policy_values
